88fa1a3608604ce3e06a1ad4031b03ad9425a64a
yuqianliu <yuqian.liu@outlook.com>
2018-02-07 23:19:20 -0500

    updates

--- mainQ3_Yuqian.R
+++ mainQ3_Yuqian.R
@@ -17,4 +17,25 @@ population <- function(days){
 }
 #nls from R
 mytab3 <- nls(N ~ K*N0/(N0+(K-N0)*exp(-r*tt)),
-              start = list(K = 2000, r = 0.1))
+              start = list(K = 2000, r = 0.1), trace = TRUE)
+
+## Problem 3.b Countour residual error
+#opti: K = 1049.4074581 r = 0.1182683
+#range: K from 800 to 1300, r from 0.07 to 0.15
+K_plot <- seq(800, 1300, length.out = 100)
+r_plot <- seq(0.07, 0.15, length.out = 100)
+error <- matrix(data = NA, nrow = length(K_plot), ncol = length(r_plot))
+residual_error <- function(K, r){
+  for (variable in vector) {
+    
+  }
+}
+for (i in 1:100) {
+  for (j in 1:100) {
+    error[j,i] <- residual_error(K_plot[j],r_plot[i])
+  }
+}
+                                 
+
+
+## Probelm 3.c
